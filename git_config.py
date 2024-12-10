c96523986e7bc0f426eaf6eb9accd18c9fcc5e36
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2024-12-10 09:17:04 +0800

    Create git_config.py

---
+++ git_config.py
@@ -0,0 +1,31 @@
+PROJECT = {
+	"ecs": {
+		"cookie": "",
+		"id": {
+			"bao-vpn-api": "938",
+			"baoluogate": "353",
+			"baoluo-vpn": "474",
+			"neuron-dashboard": "630",
+			"robot": "338",
+			"robot-wxapp": "1023"
+		},
+		"url": {
+			"commit": "https://ecs.baocloud.cn/api/v4/projects/%s/repository/commits?per_page=100&page=%s",
+			"diff": "https://ecs.baocloud.cn/api/v4/projects/%s/repository/commits/%s",
+			"change": "https://ecs.baocloud.cn/blyjs/robot/%s/-/commit/%s"
+		}
+	},
+	"gitlab": {
+		"cookie": "",
+		"id": {
+			"baoluo-app": "2865",
+			"baoluo-wxapp": "3171",
+			"baorobot-manager": "2791"
+		},
+		"url": {
+			"commit": "https://gitlab.baocloud.cn/gyhlw/api/v4/projects/%s/repository/commits?per_page=100&page=%s",
+			"diff": "https://gitlab.baocloud.cn/gyhlw/api/v4/projects/%s/repository/commits/%s",
+			"change": "https://gitlab.baocloud.cn/gyhlw/BAOROBOT/%s/-/commit/%s"
+		}
+	}
+}
